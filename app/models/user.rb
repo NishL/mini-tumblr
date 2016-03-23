60693162e695fb30fc52875044dec2d191f27047
Nish <lalgee.nish@gmail.com>
2016-03-22 20:00:20 -0400

    Complete follow and follower in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  
+  #Allow users to follow and be followed using act_as_follower gem
+  acts_as_followable
+  acts_as_follower
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates_presence_of :username
